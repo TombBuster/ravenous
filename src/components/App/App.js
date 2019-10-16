bfd8391bd0d9e9395fe5691621b7597f24ec8ce9
Tom Bennett <thomas.bennett@williamhill.com>
2019-10-16 13:32:53 +0100

    finished part 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    );
+  }
 }
 
 export default App;
