8f3163a54cf6146b1d35d7bb2baa1e2a39210217
Tom Bennett <thomas.bennett@williamhill.com>
2019-10-17 10:36:33 +0100

    finished project

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,8 +6,8 @@ class BusinessList extends React.Component {
     render() {
         return (
             <div className="BusinessList">
-                {this.props.businesses.map((business) => {
-                    return <Business business={business} />;
+                {this.props.businesses.map(business => {
+                    return <Business key={business.id} business={business} />;
                 })
                 }
             </div>
